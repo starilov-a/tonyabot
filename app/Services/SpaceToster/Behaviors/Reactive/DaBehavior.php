a45a4ab2bc3dbd542548e9d6b1e07a123520766c
starilov <anton_starilov@mail.ru>
2023-12-14 01:06:43 +0300

    улучшение daBehavior

--- app/Services/SpaceToster/Behaviors/Reactive/DaBehavior.php
+++ app/Services/SpaceToster/Behaviors/Reactive/DaBehavior.php
@@ -12,7 +12,7 @@ use App\Services\Telegram;
 class DaBehavior extends AbstarctReactiveBehavior implements MessageBehavior
 {
     protected $code = 'da';
-    private $dictionary = ['да','Да','дА','DA','da','Da','dA','Да.','дА.','DA.','da.','Da.','dA.','да?'];
+    private $dictionary = ['да','да.','да?','da','da.','da?'];
 
     public function message(Telegram $telegram): void
     {
@@ -21,7 +21,9 @@ class DaBehavior extends AbstarctReactiveBehavior implements MessageBehavior
 
         $messageOutput = 'Меня заставили это делать...';
         foreach ($this->behaviorMessages as $message) {
-            if (in_array($message->text,$this->dictionary)) {
+            //TODO переделать поиск "да"
+            $text = mb_strtolower($message->text);
+            if (in_array($text,$this->dictionary)) {
                 $chat = $message->chat->id;
                 $message_id = $message->id;
                 $update_id = $message->telegram_update_id;
